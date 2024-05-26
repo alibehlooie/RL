953bf51a6b40b5d69f6f95547f61309321eeb49b
Sadjad Zamani <zamani.saja@gmail.com>
2024-05-26 12:45:54 +0200

    dev

--- sb3_train.py
+++ sb3_train.py
@@ -4,25 +4,36 @@
     Read the stable-baselines3 documentation and implement a training
     pipeline with an RL algorithm of your choice between PPO and SAC.
 """
+import argparse
+
 import gym
 from env.custom_hopper import *
 
 from stable_baselines3 import SAC
 
-def main():
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n-episodes', default=10000, type=int, help='Number of training episodes')
+    parser.add_argument('--print-every', default=10, type=int, help='Print info every <> episodes')
+    parser.add_argument('--save', default="model", type=str, help='Save model as ...')
+
+    return parser.parse_args()
+
+
+
+def main(args):
     train_env = gym.make('CustomHopper-source-v0')
 
     print('State space:', train_env.observation_space)  # state-space
     print('Action space:', train_env.action_space)  # action-space
     print('Dynamics parameters:', train_env.get_parameters())  # masses of each
     
-    model = SAC("MlpPolicy", train_env, verbose=1)
-    model.learn(total_timesteps=10000, log_interval=4)
-    model.save("sac_custom_hopper")
 
-    #
-    # TASK 4 & 5: train and test policies on the Hopper env with stable-baselines3
-    #
+    model = SAC("MlpPolicy", train_env, verbose=1)
+    model.learn(total_timesteps = args.n_episodes, log_interval = args.print_every)
+
+    model.save(args.save)
 
 if __name__ == '__main__':
-    main()
+    args = parse_args()
+    main(args)
