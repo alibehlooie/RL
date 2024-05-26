31b911cffdeb32db996f692076b1eddbd42f5284
Sadjad Zamani <zamani.saja@gmail.com>
2024-05-26 17:04:39 +0200

    argparse

--- sb3_test.py
+++ sb3_test.py
@@ -1,27 +1,32 @@
+import argparse
 
 import gym
 from stable_baselines3 import SAC
 from env.custom_hopper import *
 
 
-env = gym.make('CustomHopper-source-v0')
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', default="model", type=str, help='Read model from ...')
+    parser.add_argument('--n-episodes', default=100, type=int, help='Number of episodes to test')
+    parser.add_argument('--render', default=True, type=bool, help='Render the environment')
+
+    return parser.parse_args()
 
 
-def main():
+def main(args):
     env = gym.make('CustomHopper-source-v0')
 
     print('State space:', env.observation_space)  # state-space
     print('Action space:', env.action_space)  # action-space
     print('Dynamics parameters:', env.get_parameters())  # masses of each
 
-    model = SAC.load("models/sac_custom_hopper")
-    
-    
+    model = SAC.load(args.model)
 
-    n_episodes = 100
-    render = True
+    n_episodes = args.n_episodes
+    render = args.render
 
-    for episode in range(n_episodes):
+    for _ in range(n_episodes):
         done = False
         state = env.reset()
 
@@ -33,4 +38,5 @@ def main():
                 env.render()
 
 if __name__ == '__main__':
-    main()
+    args = parse_args()
+    main(args)
