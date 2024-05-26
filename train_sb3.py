0e9ec4a95f35f3f80fdce430a578900e15324e9c
Sadjad Zamani <zamani.saja@gmail.com>
2024-05-26 12:24:55 +0200

    dev

--- train_sb3.py
+++ train_sb3.py
@@ -7,12 +7,18 @@
 import gym
 from env.custom_hopper import *
 
+from stable_baselines3 import SAC
+
 def main():
     train_env = gym.make('CustomHopper-source-v0')
 
     print('State space:', train_env.observation_space)  # state-space
     print('Action space:', train_env.action_space)  # action-space
-    print('Dynamics parameters:', train_env.get_parameters())  # masses of each link of the Hopper
+    print('Dynamics parameters:', train_env.get_parameters())  # masses of each
+    
+    model = SAC("MlpPolicy", train_env, verbose=1)
+    model.learn(total_timesteps=10000, log_interval=4)
+    model.save("sac_custom_hopper")
 
     #
     # TASK 4 & 5: train and test policies on the Hopper env with stable-baselines3
