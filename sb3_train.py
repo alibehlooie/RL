8202ca6a5841c8671dc86642694d101870cab89b
Sadjad Zamani <zamani.saja@gmail.com>
2024-06-04 14:19:31 +0200

    cleanup

--- sb3_train.py
+++ sb3_train.py
@@ -32,56 +32,15 @@ def parse_args():
     return parser.parse_args()
 
 
-class RewardTrackerCallback(BaseCallback):
-    """
-    Custom callback for tracking rewards during training.
-    """
-    def __init__(self, check_freq: int = 10, verbose : int = 1):
-        super(RewardTrackerCallback, self).__init__(verbose)
-        self.check_freq = check_freq
-        self.rewards = []  # List to store episode rewards
-        self.steps_count_list = []
-        self.verbose = verbose
-        self.current_episode_reward = 0
-        self.steps_count = 0
-
-    def _on_training_start(self):
-        """
-        This method is called before the first rollout starts.
-        """
-        print("Training started")
-        pass
-
-    def _on_step(self) -> bool:
-        self.current_episode_reward += self.locals["rewards"][0]
-        self.steps_count += 1
-        
-        # Check for the end of an episode
-        if self.locals['dones'][0]:
-            self.rewards.append(self.current_episode_reward)  # Append reward at episode end
-            self.steps_count_list.append(self.steps_count) # Append steps count at episode end
-            self.current_episode_reward = 0  # Reset for the next episode
-            self.steps_count = 0 # Reset for the next episode
-        return True
-    
-    def _on_rollout_end(self) -> None:
-        """
-        This event is triggered before updating the policy.
-        """
-        # print("Rollout end (Agent finished an episode)")
-        pass
+def RandomizedEnv(env, u = 1):
+    r = np.random.uniform(low = -u, high = u, size = 3)
+    env.sim.model.body_mass[1] = env.sim.model.body_mass[1] + r[0]
+    env.sim.model.body_mass[2] = env.sim.model.body_mass[2] + r[1]
+    env.sim.model.body_mass[3] = env.sim.model.body_mass[3] + r[2]
+    return env
 
 
 def main(args):
-
-    # print('State space:', train_env.observation_space)  # state-space
-    # print('Action space:', train_env.action_space)  # action-space
-    # print('Dynamics parameters:', train_env.get_parameters())  # masses of each
-    
-     
-    # Use the RewardTrackerCallback
-    reward_tracker = RewardTrackerCallback(check_freq = args.callback_freq, verbose = args.verbose)
-
     # Create a hyperparameter-grid-search
     hyperparameters = {
          "learning_rate": [1e-3],
@@ -141,11 +100,6 @@ def main(args):
                     plt.ylabel('Mean Reward')
                     plt.title(name)
                     plt.savefig(reward_pic)
-
-
-    # Optional: Early Stopping
-    # stop_callback = StopTrainingOnRewardThreshold(reward_threshold=2500, verbose=1)
-    # eval_callback = EvalCallback(eval_env, callback_on_new_best=stop_callback, verbose=1)
 
 
 
