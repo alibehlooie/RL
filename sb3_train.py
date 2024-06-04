e84d78919735fe8dbb75f838c1d6fddf5d7db9ba
Sadjad Zamani <zamani.saja@gmail.com>
2024-06-04 14:20:44 +0200

    RandomizedEnv function

--- sb3_train.py
+++ sb3_train.py
@@ -47,59 +47,64 @@ def main(args):
          "gamma": [0.995],
          "tau" : [ 0.01],
          "ent_coef" : ["auto"],
+         "rand_u" : [1, 0.5, 0.2]
     }
     
     for lr in hyperparameters["learning_rate"]:
         for gamma in hyperparameters["gamma"]:
             for tau in hyperparameters["tau"]:
                 for ent_coef in hyperparameters["ent_coef"]:
+                    for u in hyperparameters["rand_u"]:
                     
-                    if(args.env == 'source'):
-                        train_env = gym.make('CustomHopper-source-v0')
-                        eval_env = gym.make("CustomHopper-source-v0")
-                    elif(args.env == 'target'):
-                        train_env = gym.make('CustomHopper-target-v0')
-                        eval_env = gym.make("CustomHopper-target-v0")
+                        if(args.env == 'source'):
+                            train_env = gym.make('CustomHopper-source-v0')
+                            eval_env = gym.make("CustomHopper-source-v0")
+                        elif(args.env == 'target'):
+                            train_env = gym.make('CustomHopper-target-v0')
+                            eval_env = gym.make("CustomHopper-target-v0")
                     
-                    eval_env = DummyVecEnv([lambda: eval_env])
+                        eval_env = DummyVecEnv([lambda: eval_env])
                     
-                    name = "SAC" + "_steps_" + str(args.n_steps) + "_lr_" + str(lr) + "_gamma_" + str(gamma) + "_tau_" + str(tau) + "_ent_coef_" + str(ent_coef)
-                    print("Training " + name)
-                    
-                    dir_name = "SAC-hyper-eval_callback/" + name + "/"
-                    reward_pic = "Reward_Pics/" +  name + ".png"
-                    
-                    # check if rewardPic file already exists
-                    if os.path.isfile(reward_pic):
-                        print("File already exists, not training again")
-                        continue
+                        # Randomize the environment
+                        train_env = RandomizedEnv(train_env, u = u)
 
-                    eval_callback = EvalCallback(
-                        eval_env,
-                        n_eval_episodes=10,   # Number of episodes to evaluate 
-                        eval_freq=1000,       # Evaluate every 1000 steps 
-                        log_path= dir_name,   # Where to log results (if desired)
-                        best_model_save_path= dir_name, # Where to save the best model (if desired)
-                        deterministic=True    # Use deterministic actions for evaluation
-                        )
+                        name = "SAC" + "_steps_" + str(args.n_steps) + "_lr_" + str(lr) + "_gamma_" + str(gamma) + "_tau_" + str(tau) + "_ent_coef_" + str(ent_coef) + "_u_" + str(u)
+                        print("Training " + name)
+                    
+                        dir_name = "SAC-hyper-eval_callback/" + name + "/"
+                        reward_pic = "Reward_Pics/" +  name + ".png"
+                    
+                        # check if rewardPic file already exists
+                        if os.path.isfile(reward_pic):
+                            print("File already exists, not training again")
+                            continue
+
+                        eval_callback = EvalCallback(
+                            eval_env,
+                            n_eval_episodes=10,   # Number of episodes to evaluate 
+                            eval_freq=1000,       # Evaluate every 1000 steps 
+                            log_path= dir_name,   # Where to log results (if desired)
+                            best_model_save_path= dir_name, # Where to save the best model (if desired)
+                            deterministic=True    # Use deterministic actions for evaluation
+                            )
 
     
-                    model = SAC("MlpPolicy", train_env, learning_rate=lr, gamma=gamma, tau=tau, ent_coef=ent_coef, verbose = args.verbose-1)
-                    model.learn(total_timesteps=args.n_steps, callback=eval_callback, progress_bar= True)
-                    print("Training finished")
+                        model = SAC("MlpPolicy", train_env, learning_rate=lr, gamma=gamma, tau=tau, ent_coef=ent_coef, verbose = args.verbose-1)
+                        model.learn(total_timesteps=args.n_steps, callback=eval_callback, progress_bar= True)
+                        print("Training finished")
 
-                    eval_results = np.load(dir_name + '/evaluations.npz') 
-                    rewards = eval_results['results'][:, 0]  
-                    lengths = eval_results['results'][:, 1]
-                    timesteps = eval_results['timesteps']   
+                        eval_results = np.load(dir_name + '/evaluations.npz') 
+                        rewards = eval_results['results'][:, 0]  
+                        lengths = eval_results['results'][:, 1]
+                        timesteps = eval_results['timesteps']   
 
 
-                    plt.figure(figsize=(10, 6))
-                    plt.plot(timesteps, rewards)
-                    plt.xlabel('Timesteps')
-                    plt.ylabel('Mean Reward')
-                    plt.title(name)
-                    plt.savefig(reward_pic)
+                        plt.figure(figsize=(10, 6))
+                        plt.plot(timesteps, rewards)
+                        plt.xlabel('Timesteps')
+                        plt.ylabel('Mean Reward')
+                        plt.title(name)
+                        plt.savefig(reward_pic)
 
 
 
